04add2e8b9fe2a8d435b8503cd080002a00309be
dmaberlin1 <dmaberlin77@gmail.com>
2023-06-23 22:11:12 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom/client';
 import {Provider} from "react-redux";
 import './index.css';
 import App from './App';
-import {store} from "./store";
+import {store,persistor} from "./store";
+import { PersistGate } from 'redux-persist/integration/react'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
 <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
     <App />
+    </PersistGate>
 </Provider>
   </React.StrictMode>
 );
--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,16 @@
 import {createStore} from 'redux'
 import {rootReducer} from "./root-reducer";
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
 
-export const store=createStore(rootReducer);
+const persistConfig={
+    key:"root",
+    storage,
+    // whitelist:['filters','positions'],
+    blacklist:['positions']
+}
+
+const persistedReducer=persistReducer(persistConfig,rootReducer)
+
+export const store=createStore(persistedReducer);
+export const persistor=persistStore(store);
